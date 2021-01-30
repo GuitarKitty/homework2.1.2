b07ffd1059adb70521c42208fef5401afb978e31
Vyacheslav Efimenko <sakhguitarkitty@icloud.com>
2021-01-30 17:49:02 +1100

    update main logic from if to switch

--- homework2.1.2/ViewController.swift
+++ homework2.1.2/ViewController.swift
@@ -7,45 +7,56 @@
 
 import UIKit
 
+enum CurrentColor {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
     @IBOutlet var RedTraficLight: UIView!
-    
     @IBOutlet var YellowTraficLight: UIView!
     @IBOutlet var GreenTraficLight: UIView!
+    
     @IBOutlet var SwitchingTraficLightButton: UIButton!
-
-       
+    private var currentColor = CurrentColor.red
+    private let lightIsOff: CGFloat = 0.3
+    private let lightIsOn: CGFloat = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
             SwitchingTraficLightButton.layer.cornerRadius = 20
-            RedTraficLight.layer.cornerRadius = 70
-            YellowTraficLight.layer.cornerRadius = 70
-            GreenTraficLight.layer.cornerRadius = 70
-            
-        
+        RedTraficLight.alpha = lightIsOff
+        YellowTraficLight.alpha = lightIsOff
+        GreenTraficLight.alpha = lightIsOff
     }
+    
+    override func viewWillLayoutSubviews() {
+        RedTraficLight.layer.cornerRadius = RedTraficLight.frame.width / 2
+        YellowTraficLight.layer.cornerRadius = YellowTraficLight.frame.width / 2
+        GreenTraficLight.layer.cornerRadius = GreenTraficLight.frame.width / 2
+    }
+    
     @IBAction func SwithcingTraficLightPressed(_ sender: Any) {
-        if (RedTraficLight.alpha < 1) && (YellowTraficLight.alpha < 1) && (GreenTraficLight.alpha < 1){
-            RedTraficLight.alpha = 1
+    
+        switch currentColor{
+        case .red:
             SwitchingTraficLightButton.setTitle("Next", for: .normal)
-        }
-        else if (RedTraficLight.alpha == 1) && (GreenTraficLight.alpha < 1)  {
-            RedTraficLight.alpha = 0.3
-            YellowTraficLight.alpha = 1
+            GreenTraficLight.alpha = lightIsOff
+            RedTraficLight.alpha = lightIsOn
+            currentColor = .yellow
+         
+        case .yellow:
+            RedTraficLight.alpha = lightIsOff
+            YellowTraficLight.alpha = lightIsOn
+            currentColor = .green
             
+        case .green:
+            YellowTraficLight.alpha = lightIsOff
+            GreenTraficLight.alpha = lightIsOn
+            currentColor = .red
         }
-        else if (RedTraficLight.alpha < 1) && (YellowTraficLight.alpha == 1){
-            YellowTraficLight.alpha = 0.3
-            GreenTraficLight.alpha = 1
-        }
-        else if (GreenTraficLight.alpha == 1){
-            SwitchingTraficLightButton.setTitle("Start", for: .normal)
-            GreenTraficLight.alpha = 0.3
-        }
+  
         
-          
-//        
+    
         }
         
         
